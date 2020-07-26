a36f2de11c41c84b9abeebf70108035d315ab726
Dmitry Suholet <suholet@gmail.com>
2020-07-27 00:14:55 +0300

    Change API urls for production

--- src/js/constants/apiHelper.js
+++ src/js/constants/apiHelper.js
@@ -1,8 +1,8 @@
 export const api = {
-  MAIN_URL: "http://localhost:3000",
-  MAIN_URL1: "https://api.donesaur.com",
-  NEWS_URL: "http://newsapi.org/v2/",
-  NEWS_URL1: "https://praktikum.tk/news/v2/",
+  MAIN_URL1: "http://localhost:3000",
+  MAIN_URL: "https://api.donesaur.com",
+  NEWS_URL1: "http://newsapi.org/v2/",
+  NEWS_URL: "https://praktikum.tk/news/v2/",
   NEWS_API_KEY: "55c8057fdc7b439fba225761533135f9",
   PAGE_SIZE: 100,
 };
